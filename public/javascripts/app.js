27a2e2303a99eacf4b598deccba6cfb709ef0415
Karl Sanford <ksanford@ostusa.com>
2014-08-25 21:43:06 -0400

    Various Changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,22 @@
-// Angular module, defining routes for the app
+    // Angular module, defining routes for the app
+
+function toolsMessage() {
+    if ((window.outerHeight - window.innerHeight) > 250) window.location = '#/dev' ;
+}
+
 angular.module('polls', ['pollServices']).
-	config(['$routeProvider', function($routeProvider) {
+	config(['$routeProvider', function ($routeProvider) {
 		$routeProvider.
 			when('/polls', { templateUrl: 'partials/list.html', controller: PollListCtrl }).
 			when('/poll/:pollId', { templateUrl: 'partials/item.html', controller: PollItemCtrl }).
 			when('/new', { templateUrl: 'partials/new.html', controller: PollNewCtrl }).
+            when('/dev', { templateUrl: 'partials/dev.html' }).
 			// If invalid route, just redirect to the main list view
 			otherwise({ redirectTo: '/polls' });
+		if (!toolsMessage()) {
+		    window.onresize = function () {
+		        toolsMessage();
+		    }
+		}
 	}]);
 	
